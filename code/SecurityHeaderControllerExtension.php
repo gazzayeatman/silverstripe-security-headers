023c8ecc1bc416f3e572d6140bc84e979ab88bf8
Garry Yeatman <garry@yeatman.co.nz>
2025-01-09 08:38:13 +1100

    Update

--- code/SecurityHeaderControllerExtension.php
+++ code/SecurityHeaderControllerExtension.php
@@ -16,10 +16,6 @@ class SecurityHeaderControllerExtension extends Extension
         $xHeaderMap = (array) Config::inst()->get('Guttmann\SilverStripe\SecurityHeaderControllerExtension', 'x_headers_map');
 
         foreach ($headersToSend as $header => $value) {
-            if ($header === 'Content-Security-Policy' && !$this->browserHasWorkingCSPImplementation()) {
-                continue;
-            }
-
             $response->addHeader($header, $value);
 
             if (isset($xHeaderMap[$header])) {
@@ -29,31 +25,4 @@ class SecurityHeaderControllerExtension extends Extension
             }
         }
     }
-
-    private function browserHasWorkingCSPImplementation()
-    {
-        $agent = strtolower(
-            $this->owner->getRequest()->getHeader('User-Agent')
-        );
-
-        if (strpos($agent, 'safari') === false) {
-            return true;
-        }
-
-        $split = explode('version/', $agent);
-
-        if (!isset($split[1])) {
-            return true;
-        }
-
-        $version = trim($split[1]);
-        $versions = explode('.', $version);
-
-        if (isset($versions[0]) && $versions[0] <= 5) {
-            return false;
-        }
-
-        return true;
-    }
-
 }
